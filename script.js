29f66ce4b2afde5c72a54dc29e75ce2b054ac509
John Pavley <jpavley@gmail.com>
2021-11-20 12:21:30 -0500

    basic drawing and animation

--- script.js
+++ script.js
@@ -6,16 +6,30 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-function animate() {
+let size = 0;
+let posX = 0;
+let posY = 0;
 
+function drawFlower() {
   ctx.fillStyle = 'red';
   ctx.strokeStyle = 'blue';
   ctx.lineWidth = 5;
   ctx.beginPath();
-  ctx.arc(100, 300, 50, 0, Math.PI * 2);
+  ctx.arc(posX, posY, size, 0, Math.PI * 2);
   ctx.closePath();
   ctx.fill();
   ctx.stroke();
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  size += 0.15;
+  posX += 0.2;
+  posY += 0.2;
+
+  drawFlower();
+
 
   requestAnimationFrame(animate);
 }
