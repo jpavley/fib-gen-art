b18b80e516b8baa63d0b52d4bcdff47e4a3cead0
John Pavley <jpavley@gmail.com>
2021-11-20 12:11:56 -0500

    animation function

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+function animate() {
+
+  ctx.fillStyle = 'red';
+  ctx.strokeStyle = 'blue';
+  ctx.lineWidth = 5;
+  ctx.beginPath();
+  ctx.arc(100, 300, 50, 0, Math.PI * 2);
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+
+  requestAnimationFrame(animate);
+}
+
+animate();
