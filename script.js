2c2be41b23730c634e8934eda95fb59aefcefc32
John Pavley <jpavley@gmail.com>
2021-11-21 10:21:35 -0500

    nice orbiting particle

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let size = 0;
-let posX = 0;
-let posY = 0;
+let posX = canvas.width / 2;
+let posY = canvas.height / 2;
+let angle = 0;
 
 function drawFlower() {
   ctx.fillStyle = 'red';
   ctx.strokeStyle = 'blue';
   ctx.lineWidth = 5;
   ctx.beginPath();
-  ctx.arc(posX, posY, size, 0, Math.PI * 2);
+  ctx.arc(posX, posY, 20, 0, Math.PI * 2);
   ctx.closePath();
   ctx.fill();
   ctx.stroke();
@@ -24,9 +24,13 @@ function drawFlower() {
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  size += 0.15;
-  posX += 0.2;
-  posY += 0.2;
+  const posXrange = 5;
+  posX += posXrange * Math.sin(angle);
+
+  const posYrange = 5;
+  posY += posYrange * Math.cos(angle);
+
+  angle += 0.1;
 
   drawFlower();
 
